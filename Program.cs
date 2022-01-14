8162a547a643450ade7026c7bed305448fc014b4
James Brandao <muricijames@gmail.com>
2022-01-13 21:21:02 -0300

    Console dos artigos

--- Program.cs
+++ Program.cs
@@ -1,4 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Orientacao_Objetos.ConteudoContexto;
 
-Console.WriteLine("Hello, World!");
+var artigos = new List<Artigo>();
+
+artigos.Add(new Artigo("Artigo sobre OOP", "orientacao-objetos"));
+artigos.Add(new Artigo("Artigo sobre C#", "csharp"));
+artigos.Add(new Artigo("Artigo sobre .NET", "dotnet"));
+
+foreach (var artigo in artigos)
+{
+    Console.WriteLine(artigo.Titulo);
+    Console.WriteLine(artigo.Url);
+}
