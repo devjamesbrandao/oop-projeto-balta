4586c9f32b1ee3f66eb11d0950d04c26b46b7fc2
James Brandao <muricijames@gmail.com>
2022-01-13 21:21:17 -0300

    Entidade artigo

---
+++ ConteudoContexto/Artigo.cs
@@ -0,0 +1,11 @@
+namespace Orientacao_Objetos.ConteudoContexto
+{
+    public class Artigo : Conteudo
+    {
+        public Artigo(string titulo, string url)
+            : base(titulo, url)
+        {
+
+        }
+    }
+}
