100c8be6ab21fc2b812fa0a2d35627d1c69704c7
James Brandao <muricijames@gmail.com>
2022-01-18 21:16:26 -0300

    Plano

---
+++ InscricaoContexto/Plano.cs
@@ -0,0 +1,10 @@
+using Orientacao_Objetos.ContextoCompartilhado;
+
+namespace Orientacao_Objetos.InscricaoContexto
+{
+    public class Plano : Base
+    {
+        public string Titulo { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
