993f377030cc75a8bd8928ca0f6233f74885d27e
James Brandao <muricijames@gmail.com>
2022-01-18 21:16:34 -0300

    Usuário

---
+++ InscricaoContexto/Usuario.cs
@@ -0,0 +1,10 @@
+using Orientacao_Objetos.ContextoCompartilhado;
+
+namespace Orientacao_Objetos.InscricaoContexto
+{
+    public class Usuario : Base
+    {
+        public string Nome { get; set; }
+        public string Senha { get; set; }
+    }
+}
